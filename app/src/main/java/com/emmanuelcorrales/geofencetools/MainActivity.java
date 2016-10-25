76cb11f8d2289a17a0c9a541cad14fe2adfbfb79
Emmanuel Corrales <contactme@emmanuelcorrales.com>
2016-10-25 17:47:02 +0800

    Show maps on start.

--- app/src/main/java/com/emmanuelcorrales/geofencetools/MainActivity.java
+++ app/src/main/java/com/emmanuelcorrales/geofencetools/MainActivity.java
@@ -1,13 +1,28 @@
 package com.emmanuelcorrales.geofencetools;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
     }
 }
