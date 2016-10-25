977b1944f9b26cd1f5cc7f027d0700a244484009
Emmanuel Corrales <contactme@emmanuelcorrales.com>
2016-10-25 21:00:21 +0800

    Remove unused import.

--- app/src/main/java/com/emmanuelcorrales/geofencetools/GeofenceEditorActivity.java
+++ app/src/main/java/com/emmanuelcorrales/geofencetools/GeofenceEditorActivity.java
@@ -3,7 +3,6 @@ package com.emmanuelcorrales.geofencetools;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
