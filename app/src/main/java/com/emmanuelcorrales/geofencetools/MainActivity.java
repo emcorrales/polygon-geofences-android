49f8a7ce923f1622cdf8b318b0c809101eb629fe
Emmanuel Corrales <contactme@emmanuelcorrales.com>
2016-10-26 17:51:25 +0800

    Removed the Fab button. Removed the dialog shown whenever the map is clicked for a long time. Show a marker after a long click on the map. Draw the polygon if there are more than 2 markers.

--- app/src/main/java/com/emmanuelcorrales/geofencetools/MainActivity.java
+++ app/src/main/java/com/emmanuelcorrales/geofencetools/MainActivity.java
@@ -3,26 +3,33 @@ package com.emmanuelcorrales.geofencetools;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ArrayAdapter;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener,
-        OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+        OnMapReadyCallback, GoogleMap.OnMapLongClickListener, GoogleMap.OnMarkerDragListener {
     private static final int REQUEST_PERMISSION_LOCATION = 34839;
 
     private GoogleMap mMap;
+    private List<Marker> mMarkers = new ArrayList<>();
+    private Polygon mPolygon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +42,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(this);
     }
 
     @Override
@@ -75,17 +79,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onMapLongClick(LatLng latLng) {
-        final String title = latLng.latitude + ", " + latLng.longitude;
+        mMarkers.add(mMap.addMarker(new MarkerOptions().position(latLng).draggable(true)));
+        mMap.setOnMarkerDragListener(this);
+        redrawPolygon();
+    }
 
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,
-                R.array.map_long_click_todo, android.R.layout.select_dialog_singlechoice);
+    @Override
+    public void onMarkerDragStart(Marker marker) {
 
-        new AlertDialog.Builder(this)
-                .setTitle(title)
-                .setCancelable(false)
-                .setPositiveButton(android.R.string.ok, null)
-                .setNegativeButton(android.R.string.cancel, null)
-                .setSingleChoiceItems(adapter, 0, null)
-                .show();
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+        redrawPolygon();
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+
+    }
+
+    private void redrawPolygon() {
+        if (mPolygon != null) {
+            mPolygon.remove();
+        }
+        mPolygon = mMap.addPolygon(new PolygonOptions().addAll(convertMarkersToLatlng(mMarkers))
+                .strokeColor(Color.RED).fillColor(Color.BLUE));
+    }
+
+    private static List<LatLng> convertMarkersToLatlng(List<Marker> markers) {
+        List<LatLng> polygonPoints = new ArrayList<>();
+        for (Marker marker : markers) {
+            polygonPoints.add(marker.getPosition());
+        }
+        return polygonPoints;
     }
 }
