e8277ddbdd5320ba6669d7c77f4047c2446351a3
Emmanuel Corrales <emmanuelcorrales22@gmail.com>
2016-11-08 23:32:10 +0800

    Be aggressive in checking if the user is inside the polygon geofence. Start the LocationUpdateService when entering the circular geofence and stop it whenever is exits.

--- app/src/main/java/com/emmanuelcorrales/polygongeofences/GeofenceTransitionIntentService.java
+++ app/src/main/java/com/emmanuelcorrales/polygongeofences/GeofenceTransitionIntentService.java
@@ -5,7 +5,6 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Location;
 import android.media.RingtoneManager;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -13,10 +12,12 @@ import android.util.Log;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingEvent;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.maps.android.PolyUtil;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import static com.emmanuelcorrales.polygongeofences.LocationUpdateService.KEY_LATITUDE;
+import static com.emmanuelcorrales.polygongeofences.LocationUpdateService.KEY_LONGITUDE;
+import static com.emmanuelcorrales.polygongeofences.LocationUpdateService.KEY_POLYGON_POINTS_COUNT;
 
 
 public class GeofenceTransitionIntentService extends IntentService {
@@ -24,10 +25,6 @@ public class GeofenceTransitionIntentService extends IntentService {
     private static final int NOTIFICATION_ENTER = 2222;
     private static final int NOTIFICATION_EXIT = 1111;
     private static final int NOTIFICATION_DWELL = 555;
-    private static final int NOTIFICATION_INSIDE_POLYGON = 777;
-    private static final String KEY_POLYGON_POINTS_COUNT = "key_polygon_points_count";
-    private static final String KEY_LATITUDE = "key_lat";
-    private static final String KEY_LONGITUDE = "key_long";
 
     public GeofenceTransitionIntentService() {
         super(TAG);
@@ -52,16 +49,19 @@ public class GeofenceTransitionIntentService extends IntentService {
             return;
         }
 
+        intent.setClass(this, LocationUpdateService.class);
         int transition = event.getGeofenceTransition();
         switch (transition) {
             case Geofence.GEOFENCE_TRANSITION_ENTER:
                 Log.d(TAG, "Entered the geofence.");
                 showNotification("Entered the circular geofence.", NOTIFICATION_ENTER);
+                startService(intent);
                 break;
 
             case Geofence.GEOFENCE_TRANSITION_EXIT:
                 Log.d(TAG, "Exited the geofence.");
                 showNotification("Exited the circular geofence.", NOTIFICATION_EXIT);
+                stopService(intent);
                 break;
 
             case Geofence.GEOFENCE_TRANSITION_DWELL:
@@ -72,18 +72,6 @@ public class GeofenceTransitionIntentService extends IntentService {
             default:
                 Log.d(TAG, "Invalid transition: " + transition);
                 break;
-        }
-
-        List<LatLng> points = readFlattenedArrayExtras(intent);
-        Location location = event.getTriggeringLocation();
-        if (location != null) {
-            LatLng triggeredPoint = new LatLng(location.getLatitude(), location.getLongitude());
-            if (PolyUtil.containsLocation(triggeredPoint, points, true)) {
-                Log.d(TAG, "Inside the polygon geofence.");
-                showNotification("Inside the polygon geofence.", NOTIFICATION_INSIDE_POLYGON);
-            } else {
-                Log.d(TAG, "Not inside the polygon geofence.");
-            }
         }
     }
 
@@ -109,17 +97,6 @@ public class GeofenceTransitionIntentService extends IntentService {
             intent.putExtra(KEY_LATITUDE + i, points.get(i).latitude);
             intent.putExtra(KEY_LONGITUDE + i, points.get(i).longitude);
         }
-    }
-
-    private static List<LatLng> readFlattenedArrayExtras(Intent intent) {
-        List<LatLng> points = new ArrayList<>();
-        int count = intent.getIntExtra(KEY_POLYGON_POINTS_COUNT, 0);
-        for (int i = 0; i < count; i++) {
-            double latitude = intent.getDoubleExtra(KEY_LATITUDE + i, 0);
-            double longitude = intent.getDoubleExtra(KEY_LONGITUDE + i, 0);
-            points.add(new LatLng(latitude, longitude));
-        }
-        return points;
     }
 }
 
---
+++ app/src/main/java/com/emmanuelcorrales/polygongeofences/LocationUpdateService.java
@@ -0,0 +1,152 @@
+package com.emmanuelcorrales.polygongeofences;
+
+import android.Manifest;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.media.RingtoneManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.PolyUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LocationUpdateService extends Service implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
+    public static final String KEY_POLYGON_POINTS_COUNT = "key_polygon_points_count";
+    public static final String KEY_LATITUDE = "key_lat";
+    public static final String KEY_LONGITUDE = "key_long";
+
+    private static final String TAG = LocationUpdateService.class.getSimpleName();
+    private static final int NOTIFICATION_INSIDE_POLYGON = 777;
+
+    private GoogleApiClient mGoogleApiClient;
+    private List<LatLng> mPolygonPoints;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand");
+        mPolygonPoints = readFlattenedArrayExtras(intent);
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = createGoogleApiClient();
+        }
+        if (!mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.connect();
+        }
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (mGoogleApiClient == null) {
+            return;
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, createLocationRequest(), this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location == null) {
+            return;
+        }
+        String msg = "";
+        LatLng triggeredPoint = new LatLng(location.getLatitude(), location.getLongitude());
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        if (PolyUtil.containsLocation(triggeredPoint, mPolygonPoints, true)) {
+            msg += "Inside the polygon geofence.";
+            Log.d(TAG, msg);
+        } else {
+            msg += "Outside of polygon geofence.";
+            Log.d(TAG, "Outside of polygon geofence.");
+        }
+        inboxStyle.setBigContentTitle(msg);
+        inboxStyle.addLine("Accuracy: " + location.getAccuracy());
+        inboxStyle.addLine("Latitude: " + location.getLatitude());
+        inboxStyle.addLine("Longitude: " + location.getLongitude());
+        showNotification(msg, NOTIFICATION_INSIDE_POLYGON, inboxStyle);
+    }
+
+    private GoogleApiClient createGoogleApiClient() {
+        return new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    private LocationRequest createLocationRequest() {
+        LocationRequest request = new LocationRequest();
+        request.setInterval(10000);
+        request.setFastestInterval(5000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return request;
+    }
+
+    private void showNotification(String message, int notifId, NotificationCompat.InboxStyle style) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(message)
+                .setStyle(style)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(notifId, builder.build());
+    }
+
+    private static List<LatLng> readFlattenedArrayExtras(Intent intent) {
+        List<LatLng> points = new ArrayList<>();
+        int count = intent.getIntExtra(KEY_POLYGON_POINTS_COUNT, 0);
+        for (int i = 0; i < count; i++) {
+            double latitude = intent.getDoubleExtra(KEY_LATITUDE + i, 0);
+            double longitude = intent.getDoubleExtra(KEY_LONGITUDE + i, 0);
+            points.add(new LatLng(latitude, longitude));
+        }
+        return points;
+    }
+}
